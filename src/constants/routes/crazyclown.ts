4f3d5e66d08be4cdccd423b220ff98e1b61f9434
chengchuanho1105 <chengchuanho1105@gmail.com>
2025-06-14 21:15:02 +0800

    -

--- src/constants/routes/crazyclown.ts
+++ src/constants/routes/crazyclown.ts
@@ -7,17 +7,4 @@ export const crazyclownRoutes: RouteMetaInfo[] = [
     title: { zh: '首頁', en: 'Home' },
     showInNavbar: true,
   },
-  {
-    name: 'crazyclown-product',
-    path: '/crazyclown/product',
-    title: { zh: '產品介紹', en: 'Product' },
-    showInNavbar: true,
-    children: [
-      {
-        name: 'crazyclown-product-list',
-        path: '/crazyclown/product/list',
-        title: { zh: '產品列表' },
-      },
-    ],
-  },
 ]
